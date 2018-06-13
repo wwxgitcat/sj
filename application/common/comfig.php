ed6a4b4a9a09de39210b475824592fdfcf829c9f
547065931@qq.com <547065931@qq.com>
2018-06-13 20:55:46 +0800

    修改

--- application/common/comfig.php
+++ application/common/comfig.php
@@ -3,6 +3,7 @@ return [
 	'view_replace_str' => [
 	        '__CSS__'=>'/public/css',
 	        '__JS__'=>'/public/js',
+	        '__JSS__'=>'/public/js',
 	        
 	        '__IMGS__'=>'/default/'
 	],

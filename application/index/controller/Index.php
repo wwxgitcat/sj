aae2cc012c27cc1c3137094181c0b7f2c3b6641d
wwx <547065931@qq.com>
2018-06-21 17:46:01 +0800

    修改

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -8,19 +8,53 @@ class Index extends Controller
 {
     public function index()
     {
-    	$tb = new Tb();
-        $search = input("search");
-        $pos = input("pos");
+    	// $tb = new Tb();
+     //    $search = input("search");
+     //    $pos = input("pos");
 
-        $no = input("no",1);
-        $size = input("size",6);
+     //    $no = input("no",1);
+     //    $size = input("size",6);
 
-    	$res = $tb->products($search,"16,18","杭州",$no,$size);
-        $res = $res['results']['n_tbk_item'];
-        dump($res);exit;
-        // dump(json_encode($res));exit;
-        $this->assign("data",$res);
+    	// $res = $tb->products($search,"16,18","杭州",$no,$size);
+     //    $res = $res['results']['n_tbk_item'];
+     //    dump($res);exit;
+     //    $this->assign("data",$res);
         return view("index");
+        $arr = [
+            'size' => input("size",1),
+            'no' => input("no",10),
+            'search' => input("search",""),
+            'cate' => "16,18",
+            'has_coupon' => "true",
+            'ip' => "127.0.0.1",
+        ];
+        $tb = new Tb();
+        $res = $tb->dgMaterialGet($arr);
+        $res = _arr_json($res);
+        $res = json_decode($res,true);
+        $this->assign("data",$res['status']['result_list']['map_data']);
+        return view("index");
+    }
+
+    public function detail()
+    {
+        $arr = [
+            'size' => input("size",1),
+            'no' => input("no",10),
+            'search' => input("search",""),
+            'cate' => "16,18",
+            'has_coupon' => "true",
+            'ip' => "127.0.0.1",
+        ];
+        $tb = new Tb();
+        $res = $tb->dgMaterialGet($arr);
+        $res = $res->result_list->map_data;
+        echo _arr_json(200,"请求成功",$res);
+    }
+
+    public function product()
+    {
+        return view("product");
     }
 
     /**
---
+++ public/index/js/index.js
@@ -0,0 +1,61 @@
+
+$(function(){
+    $(".btn-more").click(function(){
+      
+            var url = "/index.php/index/index/detail";
+            var arr = {"search":"fz","size":1,"no":10};
+            var arr_res = a_post(url,arr);
+            $.each(arr_res.data,function(i,item){
+        　　　 　console.log(i, item);
+         　　});
+    });
+})
+
+
+function dd(argument) {
+    console.log(argument);
+}
+/**
+ * post
+ * @param  {[type]} url [description]
+ * @param  {[type]} arr [description]
+ * @return {[type]}     [description]
+ */
+function a_post(url,arr) {
+    var arr ;
+    $.ajax({
+       url : url,
+       data:arr,
+       cache : false,
+       async : false,
+       type : "POST",
+       dataType : 'json',
+       success : function (result){
+            arr = result;
+       }
+   });
+  return arr;
+}
+
+    // <ul class="am-avg-sm-2 my-shop-product-list">
+    //     <li>
+    //         <div class="am-panel am-panel-default">
+    //             <div class="am-panel-bd">
+    //                 <img class="am-img-responsive" src="{}" />
+    //                 <h3><a href="{}">xxx</a></h3>
+    //                 <div>
+    //                     <!-- <span class="list-product-price-span">￥]}</span> -->
+    //                     <span class="list-product-price-span">折扣价￥]}</span>
+    //                     <span class="list-product-commission-span">销量<br/></span>
+    //                     <!-- <span class="list-product-sorce-span">省份<br/></span> -->
+    //                 </div>
+    //                 <hr data-am-widget="divider" style="" class="am-divider am-divider-default am-cf"/>
+    //                 <ol class="am-avg-sm-3 product-list-share">
+    //                     <li><a href="#"><img src="__default__/icon1.png" class="am-img-responsive" /></a></li>
+    //                     <li><a href="#"><img src="__default__/icon2.png" class="am-img-responsive" /></a></li>
+    //                     <li><a href="#"><img src="__default__/icon3.png" class="am-img-responsive" /></a></li>
+    //                 </ol>
+    //             </div>
+    //         </div>
+    //     </li>
+    // </ul>
